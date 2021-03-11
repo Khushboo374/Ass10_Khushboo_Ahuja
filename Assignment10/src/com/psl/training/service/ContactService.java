785fd20ca808c7a0dd0a575ed585a23ad31d26c1
khushbooahuja <khushboo_ahuja@persistent.com>
2021-03-11 12:20:32 +0530

    implemented  void removeContact(Contact contact, List<Contact> contacts)

--- Assignment10/src/com/psl/training/service/ContactService.java
+++ Assignment10/src/com/psl/training/service/ContactService.java
@@ -4,13 +4,34 @@ import java.util.List;
 
 import com.psl.training.beans.Contact;
 
+@SuppressWarnings("serial")
+class ContactNotFoundException extends Exception{
+	public ContactNotFoundException() {
+		super();
+		this.getMessage();
+		this.printStackTrace();
+		// TODO Auto-generated constructor stub
+	}
+}
+
 public class ContactService {
-	void addContact(Contact contact,List<Contact> contacts) {
+	public void addContact(Contact contact,List<Contact> contacts) {
 		if(contacts.contains(contact)) {
 			System.out.println("Contact already exist in the list");
 		}
 		else {
 			contacts.add(contact);
+			System.out.println("Contact added successfully");
+		}
+	}
+	
+	public void removeContact(Contact contact, List<Contact> contacts)throws ContactNotFoundException{
+		if(contacts.contains(contact)) {
+			contacts.remove(contact);
+			System.out.println("Contact removed successfully");
+		}
+		else {
+			throw new ContactNotFoundException();
 		}
 	}
 }
